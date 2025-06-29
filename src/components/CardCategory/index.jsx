a0cacee68145a92a2c44c72a0b5fa6a4c2afb599
Kauan Rodrigues <kauanrdx145@gmail.com>
2025-06-29 12:35:34 -0300

    add CardCategory component

--- src/components/CardCategory/index.jsx
+++ src/components/CardCategory/index.jsx
@@ -1,8 +1,12 @@
 import './style.scss';
 
-export default function CardCategory() {
+export default function CardCategory({ title, color }) {
     return (
-        <>
-        </>
+        <div className="horizontal-align gap1 ai-center">
+            <div className="color-dot"
+                style={{ backgroundColor: color }}
+            ></div>
+            <span className="list-item__title">{title}</span>
+        </div>
     );
 }
